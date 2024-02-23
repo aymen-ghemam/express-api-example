1433b792b08401d300410e90aa2e40e49beeaeed
aymen ghemam <aymen.ghemam_hamed@smeetz.com>
2024-02-23 23:45:01 +0100

    add new route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const Participant = require("./models/Participant");
 const mongoose = require("mongoose");
 const { check, validationResult } = require("express-validator");
+const Team = require("./models/Team");
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -83,7 +84,63 @@ app.post(
   }
 );
 
+
+app.post(
+  "/register-clefcode",
+  [
+    // check("email", "email is required !").not().isEmpty(),
+    // check("email", "Invalid email !").isEmail(),
+    // check("name", "Fullname is required !").not().isEmpty(),
+    // check("name", "Name is not valid !").isLength({
+    //   min: 5,
+    // }),
+    // check("phone", "Invalid phone number!").not().isEmpty(),
+  ],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(200).json({ err: true, errors: errors.errors });
+
+      const exists = await Team.findOne({
+        name: req.body.teamname,
+      }).exec();
+      if (exists)
+        return res
+          .status(409)
+          .json({ err: true, errors: [{ msg: "Team name is already registered!" }] });
+
+      // const exists1 = await Participant.findOne({
+      //   phone: req.body.phone,
+      // }).exec();
+      // if (exists1)
+      //   return res
+      //     .status(200)
+      //     .json({ err: true, errors: [{ msg: "Phone nummber already registered !" }] });
+
+      const newTeam = new Team({
+        name: req.body.teamname,
+        data: req.body,
+      });
+
+      const result = await newTeam.save();
+      if (result)
+        return res
+          .status(200)
+          .json({ err: false, msg: "Successfully registered!" });
+      return res
+        .status(500)
+        .json({ err: true, errors: [{ msg: "Something went wrong!" }] });
+    } catch (err) {
+      console.log("something went wrong : " + err.message);
+      return res
+        .status(500)
+        .json({ err: true, errors: [{ msg: "Something went wrong !" }] });
+    }
+  }
+);
+
 const PORT = process.env.PORT || 3003;
 app.listen(5000, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${5000}`);
 });
